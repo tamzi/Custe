16e813d7625d338ae70b153e3702db80067d025f
tamzi <tamrefrank@gmail.com>
2023-02-03 22:51:38 +0100

    update

--- app/src/main/java/com/tamzi/custe/ui/theme/Theme.kt
+++ app/src/main/java/com/tamzi/custe/ui/theme/Theme.kt
@@ -1,7 +1,9 @@
 package com.tamzi.custe.ui.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.darkColors
+import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
 
 private val DarkColorPalette = darkColors(
@@ -25,9 +27,8 @@ private val LightColorPalette = lightColors(
     */
 )
 
-
 @Composable
-fun CusteTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable() () -> Unit) {
+fun CusteTheme(darkTheme: Boolean = isSystemInDarkTheme(), content: @Composable () -> Unit) {
     val colors = if (darkTheme) {
         DarkColorPalette
     } else {
